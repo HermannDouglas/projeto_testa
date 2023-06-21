1d6041a559a0982501b7c07c4f54a8e5aa08bd48
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2023-06-21 09:13:26 -0500

    Update admin.py

--- agenda/admin.py
+++ agenda/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from agenda.models import Compromisso, Local, Convidado
 
 
-# class CompromissoInline(admin.TabularInline):
-#     model = Compromisso
-#
-# class ConvidadoAdmin(admin.ModelAdmin):
-#     inlines = [CompromissoInline]
+class CompromissoInline(admin.TabularInline):
+    model = Compromisso
+
+class ConvidadoAdmin(admin.ModelAdmin):
+    inlines = [CompromissoInline]
 
 admin.site.register(Compromisso)
 admin.site.register(Local)
