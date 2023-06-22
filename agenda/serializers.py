4e3c85b70f19dece796b542ce697d87107112aff
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2023-06-21 22:42:01 -0300

    criado Serializers de users, groups, local, convidado e compromisso

---
+++ agenda/serializers.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+from agenda.models import Local, Convidado, Compromisso
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
+
+
+class LocalSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Local
+        fields = ['nome', 'rua', 'numero']
+
+
+class ConvidadoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Convidado
+        fields = ['nome', 'email']
+
+
+class CompromissoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Compromisso
+        fields = ['descricao', 'data_inicio', 'data_fim', 'local', 'convidados']
--- projeto_testa/urls.py
+++ projeto_testa/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from agenda.views import UserViewSet, GroupViewSet, LocalViewSet, ConvidadoViewSet, CompromissoViewSet
+
+router = routers.DefaultRouter()
+router.register('users', UserViewSet)
+router.register('groups', GroupViewSet)
+router.register('local', LocalViewSet)
+router.register('convidado', ConvidadoViewSet)
+router.register('compromisso', CompromissoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
 ]
